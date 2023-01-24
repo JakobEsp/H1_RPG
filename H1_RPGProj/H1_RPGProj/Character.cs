1d52495abef0daa645fc21051754a3cc896a63ac
JakobEsp <jakobespersen0@gmail.com>
2023-01-24 22:01:06 +0100

    Changes

---
+++ H1_RPGProj/H1_RPGProj/Character.cs
@@ -0,0 +1,45 @@
+﻿
+namespace H1_RPGProj
+{
+    class Character
+    {
+        private int level;
+        private string playerName;
+        private string characterClass;
+        private string race;
+        private DateTime birthDate;
+        private TimeSpan age;
+        private Stats stats;
+
+        public Character(int level, string playerName, string characterClass, string race, DateTime birthDate, Stats stats)
+        {
+            this.level = level;
+            this.playerName = playerName;
+            this.characterClass = characterClass;  
+            this.race = race;
+            this.birthDate = birthDate;
+            this.age = DateTime.Now - this.birthDate;
+            this.stats = stats;
+        }
+    }
+    class Stats
+    {
+        
+        private int health;
+        private int strength;
+        private int dexterity;
+        private int constitution;
+        private int intelligence;
+        private int wisdom;
+
+        public Stats(int health, int strength, int dexterity, int constitution, int intelligence, int wisdom)
+        {
+            this.health = health;
+            this.strength = strength;
+            this.dexterity= dexterity;
+            this.constitution = constitution;
+            this.intelligence = intelligence;
+            this.wisdom = wisdom;
+        }
+    }
+}
---
+++ H1_RPGProj/H1_RPGProj/Program.cs
@@ -0,0 +1,51 @@
+﻿using H1_RPGProj;
+using System.Diagnostics.Metrics;
+
+namespace H1_RPG
+{
+    class Program
+    {
+        static Character player;
+        static string[] classes = { "Soldier", "Assassin", "Hunter", "Berserker", "Enchanter", "Shadowknight" };
+        static void Main(string[] args)
+        {
+            GetPlayerInfo();
+        }
+        static void GetPlayerInfo()
+        {
+            Console.WriteLine("Name: ");
+            while(true)
+            {
+                string name = Console.ReadLine();
+                if(name.Trim() == "")
+                {
+                    Console.WriteLine("give name please");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("Choose a class by pressing number\n");
+            for(int i = 0; i < classes.Length; i++)
+            {
+                Console.WriteLine($"{i}: {classes[i]}");
+            }
+
+            while (true)
+            {
+                ConsoleKey key = Console.ReadKey().Key;
+                if(key == ConsoleKey.D0 || key == ConsoleKey.D1 || key == ConsoleKey.D2 || key == ConsoleKey.D3 || key == ConsoleKey.D4 || key == ConsoleKey.D5)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please press a valid number.");
+                }
+            }
+            Console.ReadLine();    
+        }
+    }
+}
