5fd9eae6f705443aaed193c359101c73086e719c
JakobEsp <jakobespersen0@gmail.com>
2023-01-24 23:23:12 +0100

    c

--- H1_RPGProj/H1_RPGProj/Program.cs
+++ H1_RPGProj/H1_RPGProj/Program.cs
@@ -7,16 +7,21 @@ namespace H1_RPG
     {
         static Character player;
         static string[] classes = { "Soldier", "Assassin", "Hunter", "Berserker", "Enchanter", "Shadowknight" };
+        static string[] races = { "Elf", "Dwarf", "Human", "Fearie", "Goblin" };
         static void Main(string[] args)
         {
             GetPlayerInfo();
         }
         static void GetPlayerInfo()
         {
+            string name;
+            string chosenClass;
+            string race;
+
             Console.WriteLine("Name: ");
             while(true)
             {
-                string name = Console.ReadLine();
+                name = Console.ReadLine();
                 if(name.Trim() == "")
                 {
                     Console.WriteLine("give name please");
@@ -38,6 +43,7 @@ namespace H1_RPG
                 ConsoleKey key = Console.ReadKey().Key;
                 if(key == ConsoleKey.D0 || key == ConsoleKey.D1 || key == ConsoleKey.D2 || key == ConsoleKey.D3 || key == ConsoleKey.D4 || key == ConsoleKey.D5)
                 {
+                    chosenClass = classes[(int)key - 48];
                     break;
                 }
                 else
@@ -45,6 +51,30 @@ namespace H1_RPG
                     Console.WriteLine("Please press a valid number.");
                 }
             }
+
+            Console.Clear();
+            Console.WriteLine("Choose a race by pressing number\n");
+            for (int i = 0; i < races.Length; i++)
+            {
+                Console.WriteLine($"{i}: {races[i]}");
+            }
+
+            while (true)
+            {
+                ConsoleKey key = Console.ReadKey().Key;
+                if (key == ConsoleKey.D0 || key == ConsoleKey.D1 || key == ConsoleKey.D2 || key == ConsoleKey.D3 || key == ConsoleKey.D4)
+                {
+                    race = races[(int)key - 48];
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please press a valid number.");
+                }
+            }
+
+            Console.WriteLine(chosenClass + " " + race);
+
             Console.ReadLine();    
         }
     }
